8a566d65af99f36a134eb3e5964630876056b3b3
Omkar <aomkar481@gmail.com>
2020-12-14 14:27:03 +0530

    added initial grid layout

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from '../SearchBar/SearchBar'
+import ImageList from '../ImageList/ImageList'
 import unsplash from '../../api/unsplash';
 
 class App extends React.Component{
+    state = {images : []};
+
     onSearchSubmit = async (term) => {
         
         const response = await unsplash.get('/search/photos',{
@@ -12,16 +15,18 @@ class App extends React.Component{
             }
         });
         
-        // response.data.results.forEach(photo => {
-        //     console.log(photo);
-        // });
+        this.setState({images:response.data.results});    
     }
 
     render() {
         return (
             <div className="container" style={{ marginTop:"10px"}}>
                 <SearchBar onSubmit={this.onSearchSubmit}/>
+                <div className="container" style={{ marginTop:"10px"}}>
+                    <ImageList images={this.state.images}/>
+                </div>
             </div>
+            
         );
     }
 }
---
+++ src/components/ImageCard/ImageCard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class ImageCard extends React.Component{
+    render(){
+        const {description,urls} = this.props.image;
+        return (
+            <div>
+                <img alt={description} src={urls.regular}></img>
+            </div>
+        )
+    }
+}
+
+export default ImageCard;
