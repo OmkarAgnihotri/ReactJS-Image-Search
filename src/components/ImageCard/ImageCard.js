f5ed5ee2f9de02142a4082e3e87d0974438f735e
Omkar <aomkar481@gmail.com>
2020-12-17 11:17:46 +0530

    tile display completed

--- src/components/ImageCard/ImageCard.js
+++ src/components/ImageCard/ImageCard.js
@@ -1,11 +1,27 @@
 import React from 'react';
 
 class ImageCard extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.ref = React.createRef();
+
+        this.state = {span:0}
+    }
+
+    componentDidMount(){
+        this.ref.current.addEventListener('load', () => {
+            const height = this.ref.current.clientHeight;
+            const span = Math.ceil(height/10);
+            this.setState({span:span});
+        });
+    }
+
     render(){
         const {description,urls} = this.props.image;
         return (
-            <div>
-                <img alt={description} src={urls.regular}></img>
+            <div style={{gridRowEnd : `span ${this.state.span}`}}> 
+                <img ref={this.ref} alt={description} src={urls.regular} ></img>
             </div>
         )
     }
